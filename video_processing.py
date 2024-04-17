70f64680d556b001e0d869b08bc30ce78ee91c35
daviddawha <daviddawha@gmail.com>
2024-04-16 20:05:55 -0700

    First working prototype

--- video_processing.py
+++ video_processing.py
@@ -1,10 +1,13 @@
 import cv2
+import os
 
 def process_video(video_path):
     vidcap = cv2.VideoCapture(video_path)
     success, image = vidcap.read()
     count = 0
     while success:
-        cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file      
+        cv2.imwrite("TrainingVideos/GenImages/frame%d.jpg" % count, image)     # save frame as JPEG file   
+        #img_path = f"TrainingVideos/GenImages/frame{count}.jpg"
+        #print(f"Image location: {os.path.abspath(img_path)}")   
         success, image = vidcap.read()
         count += 1
